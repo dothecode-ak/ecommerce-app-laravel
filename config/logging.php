aa40ea8163ca6e80896bf515e607bb96c18713da
anil-nodejs <sde.anilyadav@gmail.com>
2022-01-25 12:25:26 +0530

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -48,18 +48,25 @@ return [
     */
 
     'channels' => [
-        'stack' => [
+        // 'stack' => [
+        //     'driver' => 'stack',
+        //     'channels' => ['single'],
+        //     'ignore_exceptions' => false,
+        // ],
+
+        // 'single' => [
+        //     'driver' => 'single',
+        //     'path' => storage_path('logs/laravel.log'),
+        //     'level' => env('LOG_LEVEL', 'debug'),
+        // ],
+ 'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
         ],
-
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'driver' => 'errorlog',
+            'level' => 'debug',
         ],
-
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
